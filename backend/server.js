a15d17b02148974a2b48cf62344c8a5390156644
enmnauel-dev <enmnauel@gmail.com>
2026-09-06 18:33:58 -0400

    CORS: permitir cabecera Authorization en peticiones cross-origin (fix GPS desde la app mÃ³vil)

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
